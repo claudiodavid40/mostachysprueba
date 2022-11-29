94d74fffe2f6dede69c162904d427ad002a4770f
chulis <dedomartinez@solucioneschanchas.com.ar>
2022-11-28 21:26:14 -0300

    ramaramita

--- src/Header.js
+++ src/Header.js
@@ -6,6 +6,8 @@ return (
     <header className="header">
       <h1 className="header__titulo">titulo</h1>  
       <h2 className="header__titulo2">titulo 2</h2>
+      <p>aca estoy en la rama que tanto  qeuria hacer</p>
+      <p>le sigo agrengadon cosas</p>
   <Nav/>
 
     </header>
