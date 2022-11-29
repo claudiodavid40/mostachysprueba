45874c9c5649b89a4c9583412f0a8a4124f70299
chulis <dedomartinez@solucioneschanchas.com.ar>
2022-11-28 21:45:10 -0300

    bacalao

--- src/Header.js
+++ src/Header.js
@@ -7,7 +7,7 @@ return (
       <h1 className="header__titulo">titulo</h1>  
       <h2 className="header__titulo2">titulo 2</h2>
       <p>aca estoy en la rama que tanto  qeuria hacer</p>
-      <p>le sigo agrengadon cosas</p>
+      <p>le sigo agrengadon cositsas</p>
   <Nav/>
 
     </header>
